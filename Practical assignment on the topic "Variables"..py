2eac0128325f7f08a83f81630348abfad1692299
Dmitry-Rumyantsev <61865874+Dmitry-Rumyantsev@users.noreply.github.com>
2024-09-09 17:11:14 +0300

    Add files via upload
    Практическое задание по теме "Переменные".

---
+++ Practical assignment on the topic "Variables"..py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = "1,5"
+course_name = 'Python'
+time_per_task = '0.12'+' часа'
+print('Курс:',course_name, ',всего задач:',number_of_completed_tasks,',затраченно часов:'
+      ,number_of_hours_spent,', cреднее время навыполнения:', time_per_task)
