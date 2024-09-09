beadcedeece77c6e2cdc22d13f11e81b06ed5920
Dmitry-Rumyantsev <61865874+Dmitry-Rumyantsev@users.noreply.github.com>
2024-09-09 19:04:14 +0300

    Update Practical assignment on the topic "Variables"..py
    Исправил

--- Practical assignment on the topic "Variables"..py
+++ Practical assignment on the topic "Variables"..py
@@ -1,6 +1,6 @@
 number_of_completed_tasks = 12
-number_of_hours_spent = "1,5"
+number_of_hours_spent = 1,5
 course_name = 'Python'
-time_per_task = '0.12'+' часа'
+time_per_task = 1.5/12
 print('Курс:',course_name, ',всего задач:',number_of_completed_tasks,',затраченно часов:'
       ,number_of_hours_spent,', cреднее время навыполнения:', time_per_task)
