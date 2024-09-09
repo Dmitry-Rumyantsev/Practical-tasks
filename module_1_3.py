9963e691b7a60d62ccbfb5d9ee171bbcca8eb7a5
Dmitry-Rumyantsev <61865874+Dmitry-Rumyantsev@users.noreply.github.com>
2024-09-09 18:05:33 +0300

    Add files via upload
    Практическая работа по уроку "Динамическая типизация"

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name = 'Dmitry'
+print(name)
+age = 30
+print(age)
+new_age = 30,5
+print(new_age)
+is_student = 100==100
+print(is_student)
+print('Моё имя',name,
+      "Полный возвраст",age,
+      "Возраст в данный момент",new_age,
+      "Я студент",is_student)
+# возможно не понял задание
